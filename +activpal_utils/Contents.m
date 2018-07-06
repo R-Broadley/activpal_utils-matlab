f037da7befe335f5e4e5d94be41e43fe84a41d83
Rob Broadley <git@rbroadley.co.uk>
2018-07-06 14:51:04 +0100

    Modified file structure so that packages can be created directly from repo

---
+++ +activpal_utils/Contents.m
@@ -0,0 +1,10 @@
+% ACTIVPAL_UTILS
+%
+% Files
+%   load_datx - Opens the raw data files from activpal devices
+%
+% Subpackages
+%   validation - Used internally for validating inputs
+%   helper_functions - Used internally to carry out useful functions
+
+
--- Contents.m
+++
@@ -1,6 +0,0 @@
-% ACTIVPAL_UTILS
-%
-% Files
-%   load_datx - Opens the raw data files from activpal devices
-
-
